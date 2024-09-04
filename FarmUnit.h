65170a403908ebc83eba818fe2a575f114c86ac7
Malaika <u23618583@tuks.co.za>
2024-09-04 20:53:16 +0200

    Here

--- FarmUnit.h
+++ FarmUnit.h
@@ -1,36 +1,40 @@
-#ifndef FARM_UNIT_H
-#define FARM_UNIT_H
+#ifndef FARMUNIT_H
+#define FARMUNIT_H
 
 #include <vector>
 #include <string>
+
 #include "Truck.h"
 
+using namespace std;
+
 class FarmUnit {
-private:
-    std::vector<Truck*> trucks;
+    private:
+        std::vector<Truck*> trucks;
 
-public:
-    void buyTruck(Truck* truck);
-    void sellTruck(Truck* truck);
-    void callTruck();
+    public:
+        void buyTruck(Truck* truck);
+        void sellTruck(Truck* truck);
+        void callTruck();
 
-    virtual int getTotalCapacity() = 0;
-    virtual string getCropType() = 0;
-    virtual string getSoilStateName() = 0;
+        virtual int getTotalCapacity() = 0;
+        virtual string getCropType() = 0;
+        virtual string getSoilStateName() = 0;
 
-    // Composite pattern methods
-    virtual void add(FarmUnit* unit) {}
-    virtual void remove(FarmUnit* unit) {}
+        // Composite pattern methods
+        virtual void add(FarmUnit* unit) {}
+        virtual void remove(FarmUnit* unit) {}
 
-    virtual int getCurrentAmount() = 0;
-    virtual void setCurrentAmount(int amount) = 0;
+        virtual int getCurrentAmount() = 0;
+        virtual void setCurrentAmount(int amount) = 0;
 
-    virtual ~FarmUnit();
-    FarmUnit();
+        virtual ~FarmUnit();
+        FarmUnit();
 
-    // Nobuhle - Decorator functions
-    virtual void increaseProduction() = 0;
-    virtual void harvest() = 0;
-    virtual int getLeftoverCapacity() = 0;
+        // Nobuhle - Decorator functions
+        virtual void increaseProduction() = 0;
+        virtual void harvest() = 0;
+        virtual int getLeftoverCapacity() = 0;
 };
-#endif
+
+#endif // FARMUNIT_H
