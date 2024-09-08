bfc1248dfde03d6b0fc7681f715ae5d4025ce108
ReituTheCompSciGirlie <nobuhlereitumetse@gmail.com>
2024-09-09 00:28:39 +0200

    Added Doxygen code for truck, Notifi, Soilstate

--- NotificationSystem.cpp
+++ NotificationSystem.cpp
@@ -1,26 +1,48 @@
 // Class implementation for Component 4 - Observer (Subject)
 
+/**
+ * @file NotificationSystem.cpp
+ * @brief Implementation of the NotificationSystem class (Subject in the Observer Pattern).
+ */
+
 #include "NotificationSystem.h"
 
 using namespace std;
 
+/**
+ * @brief Constructor for the NotificationSystem class.
+ * 
+ * Initializes the NotificationSystem with a message indicating its readiness to manage notifications.
+ */
 NotificationSystem::NotificationSystem() {
     std::cout << "🔔 Notification System is up and running! Ready to keep the trucks in check! 🚛" << std::endl;
 }
 
-
+/**
+ * @brief Destructor for the NotificationSystem class.
+ * 
+ * Cleans up the NotificationSystem with a message indicating its shutdown.
+ */
 NotificationSystem::~NotificationSystem() {
     std::cout << "🔕 Shutting down Notification System. All trucks have been notified of the closure. 🛑" << std::endl;
 }
 
-
+/**
+ * @brief Adds a Truck to the notification system.
+ * 
+ * @param truck Pointer to the Truck object to be added as an observer.
+ */
 void NotificationSystem::addObserver(Truck* truck) 
 {
     observers.push_back(truck);
     std::cout << "🚛 Truck added to notification system.\n";
 }
 
-
+/**
+ * @brief Removes a Truck from the notification system.
+ * 
+ * @param truck Pointer to the Truck object to be removed from the observers list.
+ */
 void NotificationSystem::removeObserver(Truck* truck) 
 {
     auto it = std::find(observers.begin(), observers.end(), truck);
@@ -30,7 +52,11 @@ void NotificationSystem::removeObserver(Truck* truck)
     }
 }
 
-
+/**
+ * @brief Notifies all registered Truck observers.
+ * 
+ * Calls the startEngine method on all Truck objects currently registered as observers.
+ */
 void NotificationSystem::notifyAll() 
 {
     if (observers.empty()) {
@@ -44,14 +70,22 @@ void NotificationSystem::notifyAll()
     }
 }
 
-
+/**
+ * @brief Notifies all registered Truck observers that fertilizer is needed.
+ * 
+ * Calls the notifyAll method to alert all trucks that the soil is dry.
+ */
 void NotificationSystem::notifyFertilizerNeeded() 
 {
     std::cout << "🌱 Notification: Soil is dry. Fertilizer trucks needed.\n";
     notifyAll();
 }
 
-
+/**
+ * @brief Notifies all registered Truck observers that storage capacity is reached.
+ * 
+ * Calls the notifyAll method to alert all trucks that the storage capacity is nearing its limit.
+ */
 void NotificationSystem::notifyStorageCapacityReached() 
 {
     std::cout << "🏗️ Notification: Storage capacity nearing limit. Delivery trucks needed.\n";
--- SoilState.cpp
+++ SoilState.cpp
@@ -1,21 +1,39 @@
 // Class implementation for Component 2 - State (State)
 
+/**
+ * @file SoilState.cpp
+ * @brief Implementation of the SoilState class (State in the State Pattern).
+ */
+
 #include "SoilState.h"
 
 #include <iostream>
 
 using namespace std;
 
-// Constructor
+/**
+ * @brief Constructor for the SoilState class.
+ * 
+ * Initializes the SoilState object with a message indicating the creation of the soil foundation.
+ */
 SoilState::SoilState() {
     cout << "[SoilState] The foundation of soil has been laid." << endl;
 }
 
-// Destructor
+/**
+ * @brief Destructor for the SoilState class.
+ * 
+ * Cleans up the SoilState object with a message indicating its destruction.
+ */
 SoilState::~SoilState() {
     cout << "[SoilState] The soil state is being relinquished, and its essence fades away." << endl;
 }
 
+/**
+ * @brief Sets the context (CropField) for the SoilState.
+ * 
+ * @param ctx Pointer to the CropField object that this SoilState will be associated with.
+ */
 void SoilState::setContext(CropField* ctx) {
     context = ctx;
 }
